e1272d2d42f312a9eb841b7b685f6213807be037
pedroregissmelo23-dev <pedroregissmelo23@gmail.com>
2026-02-06 15:26:17 -0300

    Create atividade2.py
    primeira veresão

---
+++ atividade2.py
@@ -0,0 +1,10 @@
+variavel1 = "eu sei fazer print assim" 
+print (variavel1 + ' hello word')
+variavel2 = 'e assim'
+print (variavel2 + ' hello word')
+variavel3 = "20"
+variavel4 = "6"
+print ("existem " + variavel3  + " caracteres na variavel3 e " + variavel4 + " na variavel4")
+variavel5 = 20 % 6
+print ("a divisao da variavel3 e da variavel4 e ")
+print (variavel5) 
