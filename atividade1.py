e4e91a997e022302d29bc903a8818abdf6feb3d1
pedroregissmelo23-dev <pedroregissmelo23@gmail.com>
2026-02-06 15:20:15 -0300

    Create atividade1.py
    primeira versão do codigo

---
+++ atividade1.py
@@ -0,0 +1,9 @@
+idade = 12 
+minhaidade = " minha idade "
+#print(minhaidade + str(idade))
+minhaaltura = 1.53 
+idade +=   minhaaltura
+print("e minha altura " + str(minhaaltura) + " somada minha idade resulta em  " + str(idade))
+minhaaltura2 = True
+minhaidade2 = False 
+#print ( str(minhaaltura2) + str(minhaidade2))
