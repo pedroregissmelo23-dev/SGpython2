e7ec31ecc301a7db576dbf1dc5a08af8a8e101f4
pedroregissmelo23-dev <pedroregissmelo23@gmail.com>
2026-02-13 15:34:30 -0300

    Create atividadeelse1.py

---
+++ atividadeelse1.py
@@ -0,0 +1,9 @@
+chovendo = False 
+nublado = False 
+if chovendo == True :
+  print ("levar guarda chuva")
+else : 
+  if nublado == True :
+    print ("levar guarda chuva")
+  else : 
+    print ("não levar guarda chuva")
