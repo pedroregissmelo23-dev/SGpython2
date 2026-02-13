c864e3c6ec99e7752b393b8a5dda577e8e6cb4f0
pedroregissmelo23-dev <pedroregissmelo23@gmail.com>
2026-02-13 15:04:08 -0300

    Create atividade2if.py
    jogo de flechas

---
+++ atividade2if.py
@@ -0,0 +1,8 @@
+quantidadedeflechas = 19
+quantidadedevidadoinimigo = 19
+if quantidadedevidadoinimigo > quantidadedeflechas : 
+  print ("perdeu")
+if quantidadedevidadoinimigo < quantidadedeflechas : 
+  print ("ganhou")
+if quantidadedeflechas == quantidadedevidadoinimigo :
+  print ("ganhou")
