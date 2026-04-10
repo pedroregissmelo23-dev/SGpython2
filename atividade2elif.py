876a60388b006534548be423fa356c1ee08c65c6
macacoagiota <pedroregissmelo23@gmail.com>
2026-04-10 15:57:57 -0300

    Update and rename atividade2elifincompleta to atividade2elif.py

---
+++ atividade2elif.py
@@ -0,0 +1,13 @@
+nota = float(input("digite sua nota:"))
+             
+if nota >= 9:
+    print("A")
+
+elif nota >= 8:
+    print("B")
+elif nota >= 7:
+    print("c")
+elif nota >= 6
+    print("D")
+else :
+    print("F")
