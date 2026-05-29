fc4aebce24196b828d8e43bf796d4ec4c1f25b61
macacoagiota <pedroregissmelo23@gmail.com>
2026-05-29 15:41:13 -0300

    Create atividade2sequencia.py

---
+++ atividade2sequencia.py
@@ -0,0 +1,11 @@
+lista = ["rdr2" , "vava" , "repo" , "roblox"]
+
+for jogo in lista:
+    if jogo == "rdr2" :
+        print(jogo +  " esse jogo é muito bom ")
+    elif jogo == "roblox":
+        print(jogo + " esse jogo e ruim ")
+    else :
+        print(jogo + " esse jogo e ok ")
+
+    
