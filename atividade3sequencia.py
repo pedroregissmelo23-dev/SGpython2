8d26efd80f9d771cbb9e51c11dcd319be7046fbd
macacoagiota <pedroregissmelo23@gmail.com>
2026-06-05 15:46:45 -0300

    Create atividade3sequencia.py

---
+++ atividade3sequencia.py
@@ -0,0 +1,14 @@
+palavra = input("escreva uma palavra: ")
+for letra in palavra :
+    if letra == 'p':
+        print ("p é uma letra de pedro")
+    elif letra == 'e' :
+        print ("e é uma letra de pedro")
+    elif letra == 'd' :
+        print ("d é uma letra de pedro")
+    elif letra == 'r' :
+        print ("r é uma letra de pedro")
+    elif letra == 'o' :
+        print ("o é uma letra de pedro")
+    else :
+        print (letra +" não é uma letra de pedro")
